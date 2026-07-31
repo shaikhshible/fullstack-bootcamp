ba7b75b271768217823d39f3a72bf4f964d1a79c
Shaikh Shible <shaikhshible@gmail.com>
2026-07-31 12:47:53 +0300

    Day 1: Create first React portfolio

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,26 @@
+function App() {
+  return(
+    <div>
+      <h1>Hi! I am Shaikh Shible</h1>
+      <h2>I am a Full Stack Developer</h2>
+      <p>
+        Welcome to my React portfolio
+        </p>
+
+        <h3>My SkillS</h3>
+        <ul>
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>React (learning)</li>
+          </ul>
+
+        <h3>My Goal</h3>
+        <p>
+          I want to become a Full Stack Developer and get a software engineering job in Finland.
+        </p>
+      </div>
+  );
+}
+
+export default App;
