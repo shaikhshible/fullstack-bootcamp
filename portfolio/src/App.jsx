2d661d89822119c1fca3b0ed6b3fa255b33ab985
Shaikh Shible <shaikhshible@gmail.com>
2026-08-10 02:42:39 +0300

    Day 11: Add controlled contact form

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -6,6 +6,7 @@ import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import ContactForm from "./components/ContactForm";
 
 function App() {
 
@@ -27,6 +28,7 @@ function App() {
         <Skills />
         <Projects />
         <Contact />
+        <ContactForm />
       </main>
       
       <footer>
